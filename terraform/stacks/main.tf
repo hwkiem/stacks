575aabcafb4832113cd3a8ac82d2c74b6fc3718b
Henry Kiem <hwkiem@gmail.com>
2024-04-06 16:09:13 -0700

    adding vpc configuration and updating root directory

---
+++ terraform/stacks/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+    vpc_cidr = "10.0.0.0/16"
+    azs = data.aws_availability_zones.available
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = local.vpc_cidr
+    tags = {
+        Name = "Stacks-${count.index + 1}"
+    }
+}
+
+resource "aws_subnet" "private_subnets" {
+    count = length(local.azs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = cidrsubnet(local.vpc_cidr, length(local.azs), count.index)
+    availability_zone = element(local.azs, count.index)
+    tags = {
+        Name = "pvt-subnet-${count.index + 1}"
+    }
+}
+
+resource "aws_subnet" "public_subnets" {
+    count = length(local.azs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = cidrsubnet(local.vpc_cidr, length(local.azs), length(local.azs) + count.index)
+    availability_zone = element(local.azs, count.index)
+    tags = {
+        Name = "pub-subnet-${count.index + 1}"
+    }
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "Stacks-igw"
+    }
+}
