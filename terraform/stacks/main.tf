7632ccd67dcaba67d51f497ae31d5af98aa181ad
Henry Kiem <hwkiem@gmail.com>
2024-04-06 21:54:06 -0700

    ecs setup

--- terraform/stacks/main.tf
+++ terraform/stacks/main.tf
@@ -127,44 +127,45 @@ resource "aws_ecs_task_definition" "app_task" {
 }
 
 resource "aws_security_group" "load_balancer_security_group" {
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
-  }
+    vpc_id = aws_vpc.main.id
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "aws_alb" "application_load_balancer" {
-  name               = "stacks-api-alb" #load balancer name
-  load_balancer_type = "application"
-  subnets = aws_subnet.public_subnets[*].id
-  # security group
-  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+    name               = "stacks-api-alb" #load balancer name
+    load_balancer_type = "application"
+    subnets = aws_subnet.public_subnets[*].id
+    # security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
 }
 
 resource "aws_lb_target_group" "target_group" {
-  name        = "target-group"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = "${aws_vpc.main.id}" # default VPC
+    name        = "target-group"
+    port        = 80
+    protocol    = "HTTP"
+    target_type = "ip"
+    vpc_id      = "${aws_vpc.main.id}" # default VPC
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" #  load balancer
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # target group
-  }
+    load_balancer_arn = "${aws_alb.application_load_balancer.arn}" #  load balancer
+    port              = "80"
+    protocol          = "HTTP"
+    default_action {
+        type             = "forward"
+        target_group_arn = "${aws_lb_target_group.target_group.arn}" # target group
+    }
 }
 
